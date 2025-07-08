615a0d96e9ff38444ef15ec78beb84633c8075b5
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-08 22:18:58 +0300

    Add examples with upcasting and downcasting

---
+++ src/main/java/DownCasting/App.java
@@ -0,0 +1,13 @@
+package DownCasting;
+
+import Inheritance.Employee;
+import Inheritance.Programmer;
+
+public class App {
+    public static void main(String[] args) {
+        Employee e1 = new Programmer(2500, 300);
+
+        System.out.println("Salary: " + e1.getSalary());
+        System.out.println("Bonus: " + ((Programmer) e1).getBonus());
+    }
+}
---
+++ src/main/java/UpCasting/App.java
@@ -0,0 +1,14 @@
+package UpCasting;
+
+import Inheritance.Employee;
+import Inheritance.Programmer;
+
+public class App {
+    public static void main(String[] args) {
+        Employee employee = (Employee) new Programmer(2500, 300); // не обов'язково явно
+        Employee e = new Programmer(2000, 550);
+
+        System.out.println("It is Upcasting. \nSalary: " + employee.getSalary());
+        System.out.println("It is Upcasting. \nSalary: " + e.getSalary());
+    }
+}
